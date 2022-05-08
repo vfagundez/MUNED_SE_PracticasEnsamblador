132eb3b16360d6f96a1a81551c7080ca6f6c7347
Vincitori <vfagundezpoyo@gmail.com>
2022-05-08 17:08:31 +0200

    Initial commit

---
+++ p0.asm
@@ -0,0 +1,31 @@
+;Para realizar la comparación restamos los dos número (A-B). 
+; Si al realizar la resta los dos números son iguales el resultado será cero, activándose el bit Z del registro de
+; Estado. 
+; Si al realizar la resta (suma del complemento a 2 de B) se produce un bit de acarreo el resultado es positivo
+; (A> B). Ejemplo: 3-2 = 0011 + 1110 = 1 0001. 
+; Si no se produce acarreo el resultado es negativo (A<B). Ejemplo: 2-3 = 0010 + 1101 = 0 1111. 
+            list    p=16F84A
+            include <P16F84A.INC>
+
+            org     0x0000              ;Se inicia en el origen 0
+NumA        equ     0x0C                ;Variable del número A
+NumB        equ     0x0D                ;Variable del número B
+Mayor       equ     0x0E                ;Variable que almacenará el mayor de los números
+            org     0x00                ;Vector de reset
+            goto    Inicio              ;Salto incondicional al principio del programa
+            ;org     0x05                ;Vector de interrupción
+A_menor_B   movf     NumB,W              ;No, A es menor que B
+            movwf   Mayor               ;Suma A más B
+            goto    Stop                ;Salto incondicional al final del programa
+A_mayor_B   movf    NumA,W              ;No, A es menor que B
+            movwf   Mayor               ;Suma A más B
+            goto    Stop                ;Salto incondicional al final del programa
+A_igual_B   clrf    Mayor               ;Pone a 0 el resultado
+Inicio      movf    NumB,W              ;NumB -> W (acumulador)
+            subwf   NumA,W              ;A-W -> W
+            btfsc   STATUS,Z            ;Bit de cero del registro de Estado a 1 0
+            goto    A_igual_B           ;Si
+            btfsc   STATUS,C            ;Bit de acarreo del registro de Estado a 1
+            goto    A_menor_B           ;Si
+Stop        nop                         ;Poner breakpoint de parada
+            end                         ;Fin del programa fuente
